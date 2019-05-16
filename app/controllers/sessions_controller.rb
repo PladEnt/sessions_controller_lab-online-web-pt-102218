09540d49dc4cf79c3967181c17e0d216dab41c11
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-05-16 20:50:43 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,9 @@ class SessionsController < ApplicationController
     if params[:name] != nil || params[:name] != ''
       session[:name] = params[:name]
       redirect_to '/hello.html.erb'
+    else
+      redirect_to '/new.html.erb'
+    end
   end
   def destroy
     session.delete :name
